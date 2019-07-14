0ab6ca8f5b75276b072df23a06197e76053b8275
Adam Hay <heyadam66@gmail.com>
2019-07-13 20:29:33 -0500

    finished video 9

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,38 @@ import React, {Component} from 'react';
 
 class App extends Component { 
   
+  constructor(props){
+    super(props)
+
+      this.state = {
+        starList: ['hadar', 'gacrux', 'fomalhaut']
+      }
+
+  }// end constructor
+
+
+
   render() {
+    let starListItemArray = [];
+    for (let i=0; i<this.state.starList.length; i++){
+      const starListItem = <li>{this.state.starList[i]}</li>
+      starListItemArray.push(starListItem);
+    }
+
+
+
     return (
+    <>
       <div>
-        hello
+        this.state: {JSON.stringify(this.state)}
       </div>
+      <div>
+        this.state.starList: {this.state.starList}
+      </div>
+      <ul>
+        {starListItemArray}
+      </ul>
+    </>
     );
   }
 }
