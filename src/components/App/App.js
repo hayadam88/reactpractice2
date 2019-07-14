48bdb651e8acfa0191da5794571b83a509ba84cd
Adam Hay <heyadam66@gmail.com>
2019-07-13 20:02:36 -0500

    app set up and running

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 
-import './App.css';
 
-class App extends Component {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends Component { 
+  
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
 }
 
 export default App;
